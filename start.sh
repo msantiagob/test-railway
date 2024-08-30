13b16f61b0ba7edab9630c1dc3403382958ebcfb
Santiago Bedoya <santiago@Elrepapu-Mac.local>
2024-08-30 10:35:15 -0500

    fix

--- start.sh
+++ start.sh
@@ -4,4 +4,8 @@
 # It ensures the database is migrated and then launches the server.
 
 # Check if the database is up-to-date, and run migrations if necessary
-rails s -b 0.0.0.0
+sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 3000
+rails s -p 3000 -b 0.0.0.0
+
+#sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 3000
+#sudo iptables -t nat -I PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 3000
